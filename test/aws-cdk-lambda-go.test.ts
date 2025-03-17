cc82af12213e5af475c64f724f3bca395643cbe0
kiquetal <kiquetal@gmail.com>
2025-03-17 17:39:40 -0300

    Initial commit

---
+++ test/aws-cdk-lambda-go.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkLambdaGo from '../lib/aws-cdk-lambda-go-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-lambda-go-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkLambdaGo.AwsCdkLambdaGoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
